56451cd3b81f1aea684ca06e3d9a09283d107906
HariniBeeram <143022203+HariniBeeram@users.noreply.github.com>
2023-10-25 22:18:59 -0500

    add

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Beeram Harini' });
+  res.render('mydata', { title:'Beeram Harini'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 var sum=0;
 var more =0;
+
+
 router.get('/', function(req, res, next) {
   
-  more+=1;
-  sum+=more;
+  more += 1;
+  sum += more;
 
-res.send(`sum is :${sum}`);
+res.send(`sum is: ${sum}`);
 });
 
 module.exports = router;
